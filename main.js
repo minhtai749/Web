4a34f11d42a11a25d8d6f01bb0b80eb65f10cf7e
minhtai749 <minhtai.23.1.97@gmail.com>
2022-05-31 20:57:42 +0700

    31/05/2022

---
+++ main.js
@@ -0,0 +1,31 @@
+console.log ("Hello Word");
+{
+    const GREETING = "My name is";//Constant (Khong doi)
+    console.log (GREETING);
+}
+{
+    let name = "Kira";// Co the thay doi
+    console.log (name);
+}           
+//Type of data
+{
+    let length = 100;//number
+    let ten = "T";//String
+    let x =
+    {ai:"Ai",age:2};//Object
+    let bit =true;//Bo0lean
+    let a;//Undefined
+    //function find(); //Function
+    console.log (x);
+}
+
+    const cal = (a,b)=>{
+        rs=a*b
+        console.log ("Ket qua la",rs);}
+cal(2,5);
+let mark = 2;
+switch (mark) {
+    case mark < 5:
+        Text = "Khong Dat"
+        break;
+}
